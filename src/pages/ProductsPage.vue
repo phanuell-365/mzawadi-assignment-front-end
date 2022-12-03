f85163df98b35732e961ab0c7d6b8e3af9cf2ad7
phanuell-365 <phanuelkidakwa@gmail.com>
2022-12-03 11:08:47 +0300

    Created the products form and finished setting up its modal.

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,19 +1,54 @@
 <template>
-
+  <div>
+    <CardHeading @add-click="onAddClickHandler" />
+  </div>
+  <div>
+    <ModalContainer
+      :initial-focus="initialFocusElement"
+      :show="show"
+      dialog-title="Create Product"
+    >
+      <template #content>
+        <CreateProductForm
+          @close-modal="onClose"
+          @name-input="onNameInputHandler"
+        />
+      </template>
+    </ModalContainer>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import {useRoutingStore} from "../stores/routing";
-import {RouteRecordName, useRoute} from "vue-router";
+import { useRoutingStore } from "../stores/routing";
+import { useRoute } from "vue-router";
+import { ref, Ref } from "vue";
+import CardHeading from "../components/cards/CardHeading.vue";
+import ModalContainer from "../components/modals/create/ModalContainer.vue";
+import CreateProductForm from "../components/app/products/CreateProductForm.vue";
 
 const routingStore = useRoutingStore();
 
 const route = useRoute();
 
+const show: Ref<boolean> = ref(false);
+
 // set the current route name in the routing store
-routingStore.setCurrentRoute(<RouteRecordName>route?.name as string);
+routingStore.setCurrentRoute(route?.name as string);
+
+const onAddClickHandler = () => {
+  show.value = !show.value;
+};
+
+const initialFocusElement: Ref<HTMLInputElement | null> = ref(null);
+
+const onNameInputHandler = (input: HTMLInputElement) => {
+  initialFocusElement.value = input;
+};
+
+const onClose = (value: boolean) => {
+  console.log("the value received", value);
+  show.value = value;
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
