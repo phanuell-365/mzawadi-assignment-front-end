fd97c8ffb0e9895331ba8c8158e65e7399e3c464
phanuell-365 <phanuelkidakwa@gmail.com>
2022-12-03 10:20:12 +0300

    Created the users form and finished setting up its modal.

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
-    <CardHeading />
+    <CardHeading @add-click="onAddClickHandler" />
   </div>
-  <div></div>
+  <div>
+    <ModalContainer
+      :initial-focus="initialFocusElement"
+      :show="show"
+      dialog-title="Create User"
+    >
+      <template #content>
+        <CreateUserForm
+          @close-modal="onClose"
+          @name-input="onNameInputHandler"
+        />
+      </template>
+    </ModalContainer>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { useRoutingStore } from "../stores/routing";
 import { useRoute } from "vue-router";
 import CardHeading from "../components/cards/CardHeading.vue";
+import ModalContainer from "../components/modals/create/ModalContainer.vue";
+import CreateUserForm from "../components/app/users/CreateUserForm.vue";
+import { ref, Ref } from "vue";
 
 const routingStore = useRoutingStore();
 
@@ -16,6 +32,23 @@ const route = useRoute();
 
 // set the current route name in the routing store
 routingStore.setCurrentRoute(route?.name as string);
+
+const show: Ref<boolean> = ref(false);
+
+const onAddClickHandler = () => {
+  show.value = !show.value;
+};
+
+const initialFocusElement: Ref<HTMLInputElement | null> = ref(null);
+
+const onNameInputHandler = (input: HTMLInputElement) => {
+  initialFocusElement.value = input;
+};
+
+const onClose = (value: boolean) => {
+  console.log("the value received", value);
+  show.value = value;
+};
 </script>
 
 <style scoped></style>
